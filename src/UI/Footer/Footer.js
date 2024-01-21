c3edea94dd89d62e1a8c8599e8e1c4013c0e3aec
richal3004 <richal3004@gmail.com>
2024-01-20 22:57:56 -0500

    asdnajdsfhjkdsfasf

--- src/UI/Footer/Footer.js
+++ src/UI/Footer/Footer.js
@@ -2,7 +2,7 @@ import classes from './Footer.module.css';
 const Footer=(props)=>{
     return(
     <div className={classes.Footer}>
-        Designed And coded By Harsh & Richal with Love
+        Designed coded By Harsh & Richal with Love
     </div>
     )
 
